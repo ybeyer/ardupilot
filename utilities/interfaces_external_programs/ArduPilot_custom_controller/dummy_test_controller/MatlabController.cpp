bed6442d89e1820b66d1a09aa8231f2fb93c5f10
Yannic Beyer <y.beyer@tu-bs.de>
2021-03-18 16:17:31 +0100

    Add example for a dummy test controller and adjust the Readme.

---
+++ utilities/interfaces_external_programs/ArduPilot_custom_controller/dummy_test_controller/MatlabController.cpp
@@ -0,0 +1,80 @@
+//
+// Sponsored License - for use in support of a program or activity
+// sponsored by MathWorks.  Not for government, commercial or other
+// non-sponsored organizational use.
+//
+// File: MatlabController.cpp
+//
+// Code generated for Simulink model 'MatlabController'.
+//
+// Model version                  : 1.378
+// Simulink Coder version         : 9.0 (R2018b) 24-May-2018
+// C/C++ source code generated on : Thu Mar 18 16:08:05 2021
+//
+// Target selection: ert.tlc
+// Embedded hardware selection: Intel->x86-64 (Linux 64)
+// Code generation objectives:
+//    1. Execution efficiency
+//    2. RAM efficiency
+// Validation result: Not run
+//
+#include "MatlabController.h"
+
+// Model step function
+void MatlabControllerClass::step()
+{
+  // Outport: '<Root>/logs' incorporates:
+  //   Inport: '<Root>/cmd'
+  //   Inport: '<Root>/measure'
+
+  rtY.logs[0] = rtU.cmd.roll;
+  rtY.logs[1] = rtU.cmd.pitch;
+  rtY.logs[2] = rtU.cmd.yaw;
+  rtY.logs[3] = rtU.cmd.thr;
+  rtY.logs[4] = rtU.measure.omega_Kb[0];
+  rtY.logs[7] = rtU.measure.EulerAngles[0];
+  rtY.logs[10] = rtU.measure.V_Kg[0];
+  rtY.logs[5] = rtU.measure.omega_Kb[1];
+  rtY.logs[8] = rtU.measure.EulerAngles[1];
+  rtY.logs[11] = rtU.measure.V_Kg[1];
+  rtY.logs[6] = rtU.measure.omega_Kb[2];
+  rtY.logs[9] = rtU.measure.EulerAngles[2];
+  rtY.logs[12] = rtU.measure.V_Kg[2];
+  rtY.logs[13] = 0.0F;
+  rtY.logs[14] = 0.0F;
+}
+
+// Model initialize function
+void MatlabControllerClass::initialize()
+{
+  {
+    int32_T i;
+
+    // ConstCode for Outport: '<Root>/u' incorporates:
+    //   Constant: '<S2>/Constant'
+
+    for (i = 0; i < 8; i++) {
+      rtY.u[i] = rtConstP.Constant_Value[i];
+    }
+
+    // End of ConstCode for Outport: '<Root>/u'
+  }
+}
+
+// Constructor
+MatlabControllerClass::MatlabControllerClass()
+{
+  // Currently there is no constructor body generated.
+}
+
+// Destructor
+MatlabControllerClass::~MatlabControllerClass()
+{
+  // Currently there is no destructor body generated.
+}
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
---
+++ utilities/interfaces_external_programs/ArduPilot_custom_controller/dummy_test_controller/MatlabController_data.cpp
@@ -0,0 +1,35 @@
+//
+// Sponsored License - for use in support of a program or activity
+// sponsored by MathWorks.  Not for government, commercial or other
+// non-sponsored organizational use.
+//
+// File: MatlabController_data.cpp
+//
+// Code generated for Simulink model 'MatlabController'.
+//
+// Model version                  : 1.378
+// Simulink Coder version         : 9.0 (R2018b) 24-May-2018
+// C/C++ source code generated on : Thu Mar 18 16:08:05 2021
+//
+// Target selection: ert.tlc
+// Embedded hardware selection: Intel->x86-64 (Linux 64)
+// Code generation objectives:
+//    1. Execution efficiency
+//    2. RAM efficiency
+// Validation result: Not run
+//
+#include "MatlabController.h"
+
+// Constant parameters (default storage)
+const ConstP rtConstP = {
+  // Computed Parameter: Constant_Value
+  //  Referenced by: '<S2>/Constant'
+
+  { 0.11F, 0.12F, 0.13F, 0.14F, 0.15F, 0.16F, 0.17F, 0.18F }
+};
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
