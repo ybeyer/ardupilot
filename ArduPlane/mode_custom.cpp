0904f050faa9d6b8719e518a8f0471446dbf10fd
Fabian Gücker <f.guecker@tu-braunschweig.de>
2021-05-17 14:44:40 +0200

    Update custom flight mode.
    - Add rangefinders.
    - Add initialisation function call.
    - Replace divide by multiply of pre-calculated values.
    - Replace servo channel outputs (channel 1, channel 2, ...) by function-based outputs (e.g. aileron, elevator, ...). So outputs can be routed through the ardupilot parameters as for the built-in flight modes.

--- ArduPlane/mode_custom.cpp
+++ ArduPlane/mode_custom.cpp
@@ -4,30 +4,42 @@
 
 bool ModeCustom::_enter()
 {
+    custom_controller.initialize(); 
     return true;
 }
 
 
-void ModeCustom::update() 
+void ModeCustom::update()
 {
-    
+
     // get pilot inputs
-    int16_t tr_max = 4500;
+    float tr_max_inv = 1.0 / 4500;
     float roll_out_high = plane.channel_roll->get_control_in();
-    float roll_out = roll_out_high / tr_max;
+    float roll_out = roll_out_high * tr_max_inv;
     float pitch_out_high = plane.channel_pitch->get_control_in();
-    float pitch_out = pitch_out_high / tr_max;
+    float pitch_out = pitch_out_high * tr_max_inv;
     float yaw_out_high = plane.channel_rudder->get_control_in();
-    float yaw_out = yaw_out_high / tr_max;
+    float yaw_out = yaw_out_high * tr_max_inv;
     float throttle_control_high = plane.channel_throttle->get_control_in();
-    float throttle_control = throttle_control_high / 100 * 2 - 1;
+    float throttle_control = 0.01 * throttle_control_high;
 
+    // get rangefinder distance in cm (if sensor with orientation is avail.)
 
-    // get measured inputs
+    float rangefinder_dist[6];
+    rangefinder_dist[0] = plane.rangefinder.distance_cm_orient((Rotation)1);
+    rangefinder_dist[1] = plane.rangefinder.distance_cm_orient((Rotation)2);
+    rangefinder_dist[2] = plane.rangefinder.distance_cm_orient((Rotation)3);
+    rangefinder_dist[3] = plane.rangefinder.distance_cm_orient((Rotation)4);
+    rangefinder_dist[4] = plane.rangefinder.distance_cm_orient((Rotation)5);
+    rangefinder_dist[5] = plane.rangefinder.distance_cm_orient((Rotation)6);
+
+    int   RNGFND_num_sensors = plane.rangefinder.num_sensors();
+
+      // get measured inputs
     Vector3f angular_velocity_Kb = plane.ahrs.get_gyro();
 
     Quaternion attitude_vehicle_quat;
-    bool a = plane.ahrs.get_quaternion(attitude_vehicle_quat);
+    // bool a = plane.ahrs.get_quaternion(attitude_vehicle_quat);
     // gcs().send_text(MAV_SEVERITY_DEBUG, "a %d", (int8_t)a);
     // gcs().send_text(MAV_SEVERITY_DEBUG, "test %5.3f %5.3f %5.3f %5.3f", (double)attitude_vehicle_quat[0], (double)attitude_vehicle_quat[1], attitude_vehicle_quat[2], attitude_vehicle_quat[3]);
 
@@ -50,10 +62,10 @@ void ModeCustom::update()
     // assign commanded and measured values to controller inputs struct
     ExtU rtU_;
 
-    rtU_.cmd.roll = roll_out;
+    rtU_.cmd.roll  = roll_out;
     rtU_.cmd.pitch = pitch_out;
-    rtU_.cmd.yaw = yaw_out;
-    rtU_.cmd.thr = -throttle_control;
+    rtU_.cmd.yaw   = yaw_out;
+    rtU_.cmd.thr   = throttle_control;
 
     rtU_.measure.omega_Kb[0] = angular_velocity_Kb[0];
     rtU_.measure.omega_Kb[1] = angular_velocity_Kb[1];
@@ -77,11 +89,18 @@ void ModeCustom::update()
     rtU_.measure.lla[0] = plane.current_loc.lat;
     rtU_.measure.lla[1] = plane.current_loc.lng;
     rtU_.measure.lla[2] = plane.current_loc.alt;
+    rtU_.measure.rangefinder[0] = rangefinder_dist[0];
+    rtU_.measure.rangefinder[1] = rangefinder_dist[1];
+    rtU_.measure.rangefinder[2] = rangefinder_dist[2];
+    rtU_.measure.rangefinder[3] = rangefinder_dist[3];
+    rtU_.measure.rangefinder[4] = rangefinder_dist[4];
+    rtU_.measure.rangefinder[5] = rangefinder_dist[5];
+
 
 
     // get controller outputs struct
     custom_controller.rtU = rtU_;
-    custom_controller.step(); //run a step in controller. 
+    custom_controller.step(); //run a step in controller.
     ExtY rtY_ = custom_controller.rtY;
 
     // log data
@@ -105,30 +124,57 @@ void ModeCustom::update()
         (double)custom_controller.rtY.logs[13],
         (double)custom_controller.rtY.logs[14]);
 
-
     // send controller outputs to channels and set PWMs
-    for (uint8_t i=0; i<NUM_SERVO_CHANNELS; i++) {
-        SRV_Channel::Aux_servo_function_t function_i = SRV_Channels::channel_function(i);
-        SRV_Channel *c = SRV_Channels::srv_channel(i);
-        bool is_servo = c->is_servo();
-        float u_norm = rtY_.u[i];
-        if (is_servo) {
-            u_norm = rtY_.u[i]*2-1;
+    for (uint8_t i=0; i<8; i++) {
+        SRV_Channel::Aux_servo_function_t function_i = (SRV_Channel::Aux_servo_function_t)rtY_.function_channels[i];
+
+        if(function_i == SRV_Channel::Aux_servo_function_t::k_none){
+            continue;  
         }
-        if (c->get_reversed()) {
-            u_norm = -u_norm;
-        }
-        // similar to servos.cpp, l. 714
-        if (!hal.util->get_soft_armed() && !c->is_servo()) {
-            if (plane.arming.arming_required() == AP_Arming::Required::YES_ZERO_PWM) {
-                SRV_Channels::set_output_limit(function_i, SRV_Channel::Limit::ZERO_PWM);
-            } else {
-                SRV_Channels::set_output_norm(function_i, 0);
+        if(function_i == SRV_Channel::Aux_servo_function_t::k_throttle) {
+            if (!hal.util->get_soft_armed()) {
+                if (plane.arming.arming_required() == AP_Arming::Required::YES_ZERO_PWM) {
+                    SRV_Channels::set_output_limit(function_i, SRV_Channel::Limit::ZERO_PWM);
+                } else {
+                    SRV_Channels::set_output_norm(function_i, 0);
+                }
             }
-        } else {
-            SRV_Channels::set_output_norm(function_i, u_norm);
+            else{
+                SRV_Channels::set_output_norm(function_i, rtY_.channels[i]);
+            }
+        }
+        else
+        {
+             SRV_Channels::set_output_norm(function_i, rtY_.channels[i]);
         }
     }
-    
+    // plot rangefinder distance in cm for debugging
+
+    static uint16_t counter;
+    static uint32_t last_t, last_print;
+    uint32_t now = AP_HAL::micros();
+
+    if (last_t == 0) {
+        last_t = now;
+        return;
+    }
+    last_t = now;
+
+    counter++;
+
+    if (now - last_print >= 100000 /* 100ms : 10hz */) {
+
+       GCS_SEND_TEXT(MAV_SEVERITY_DEBUG, "%d: distance_cm %f %f %f %f %f %f \n",
+        RNGFND_num_sensors,
+        rangefinder_dist[0], rangefinder_dist[1], rangefinder_dist[2],
+        rangefinder_dist[3], rangefinder_dist[4], rangefinder_dist[5]);
+        GCS_SEND_TEXT(MAV_SEVERITY_DEBUG, "Servo PWM: %f %f %f %f %f %f %f %f \n",
+         rtY_.channels[0], rtY_.channels[1], rtY_.channels[2], rtY_.channels[3],
+         rtY_.channels[4], rtY_.channels[5], rtY_.channels[6], rtY_.channels[7]);
+
+        last_print = now;
+        counter = 0;
+    }
+
 
 }
