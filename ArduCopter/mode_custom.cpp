5e1ee0c864975812218dee7dfcab8f25b7c0fc3d
Yannic Beyer <y.beyer@tu-bs.de>
2025-07-10 18:17:12 +0200

    Removal of the generated file ModeCustomParams.cpp
    This file was needed for tunable parameters of
    the Matlab controller. It caused a compiler error
    because it included Copter.h which did not work
    except ModeCustomParams.cpp was moved to the
    ArduCopter folder. The file only contains the
    definition of ModeCustom::var_info[], which was
    moved to mode_custom.cpp. mode_custom.cpp is
    located in the ArduCopter folder and already
    includes Copter.h.

--- ArduCopter/mode_custom.cpp
+++ ArduCopter/mode_custom.cpp
@@ -3,12 +3,15 @@
 #include <GCS_MAVLink/GCS.h>
 
 // Check if MatlabControllerClass contains parameters
-//  - if MatlabControllerClass was postprocessed for tunable parameters:
-//      - 'MatlabController.h' defines MODE_CUSTOM_VAR_INFO
-//      - 'ModeCustomParams.cpp' defines 'const AP_Param::GroupInfo ModeCustom::var_info'
+// If MatlabControllerClass was postprocessed for tunable parameters, use AP_SUBGROUPINFO
 #ifndef MODE_CUSTOM_VAR_INFO
 #define MODE_CUSTOM_VAR_INFO
 const AP_Param::GroupInfo ModeCustom::var_info[] = {
+	AP_GROUPEND
+};
+#else
+const AP_Param::GroupInfo ModeCustom::var_info[] = {
+  AP_SUBGROUPINFO(custom_controller, "", 0, ModeCustom, MatlabControllerClass),
 	AP_GROUPEND
 };
 #endif
